f663aa77adff62f656d93e2efe70b1c13fec6880
spnevolution <67012702+spnevolution@users.noreply.github.com>
2020-07-16 07:55:10 +0800

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,6 +1,6 @@
 const numbers = document.querySelectorAll(".number")
  numbers.forEach((number) => {
- number.addEventListener("click",()=>{
-       console.log("number is pressed")
+ number.addEventListener("click",(event)=>{
+       console.log(event,target,value)
   })
  });
