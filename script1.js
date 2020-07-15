0b924183acf4bcde175c89d117482f537abb4b0f
spnevolution <67012702+spnevolution@users.noreply.github.com>
2020-07-16 07:37:37 +0800

    Update script1.js

--- script1.js
+++ script1.js
@@ -2,4 +2,5 @@ const numbers = document.querySelectorAll(".number");
  numbers.forEach((number) => {
  number.addeventlistener("click",()=>{
        console.log("number is pressed")
-  });
+  })
+ });
